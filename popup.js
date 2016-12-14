02295732885cfaede5e75ea7676ee4fec01937e8
Filipe Dias Rodrigues <filipergues@gmail.com>
2016-12-14 22:39:24 +0000

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,163 @@
+$(document).ready(function () {
+    
+    // Substitui o ano no titulo html pela variavel ano do ucs.js
+    document.getElementById("ano").innerHTML = ano; 
+    
+    // Cria as linhas referentes às uc's utilizando os dados do ucs.js
+        // Primeiro Semestre
+    for (i = 0; i < ucs_1_semestre.length; i++) {
+        document.getElementById("primeiroSemestre").innerHTML += 
+        "<tr class='linhas' id='" + ucs_1_semestre[i][0] + "'>" +
+        "<td><input type='checkbox' class='favoritos' id='checkbox" + ucs_1_semestre[i][0] + "'></td>" +
+        "<td class='codigo'>" + ucs_1_semestre[i][0] + "</td>" +
+        "<td><a href='http://wiki.dcet.uab.pt/files/index.php/" + ucs_1_semestre[i][1] + "' target='_blank'>" + ucs_1_semestre[i][1] + "</a></td>" +
+        "<td class='normal'>" + ucs_1_semestre[i][2] + "<span class='mono'>" + " " + ucs_1_semestre[i][3] + "</span></td>" +
+        "<td class='recurso'>" + ucs_1_semestre[i][4] + "<span class='mono'>" + " " + ucs_1_semestre[i][5] + "</span></td>" +
+        "</tr>";
+    }
+    
+        // Segundo Semestre
+    for (i = 0; i < ucs_2_semestre.length; i++) {
+        document.getElementById("segundoSemestre").innerHTML += 
+        "<tr class='linhas' id='" + ucs_2_semestre[i][0] + "'>" +
+        "<td><input type='checkbox' class='favoritos' id='checkbox" + ucs_2_semestre[i][0] + "'></td>" +
+        "<td class='codigo'>" + ucs_2_semestre[i][0] + "</td>" +
+        "<td><a href='http://wiki.dcet.uab.pt/files/index.php/" + ucs_2_semestre[i][1] + "' target='_blank'>" + ucs_2_semestre[i][1] + "</a></td>" +
+        "<td class='normal'>" + ucs_2_semestre[i][2] + "<span class='mono'>" + " " + ucs_2_semestre[i][3] + "</span></td>" +
+        "<td class='recurso'>" + ucs_2_semestre[i][4] + "<span class='mono'>" + " " + ucs_2_semestre[i][5] + "</span></td>" +
+        "</tr>";
+    }
+    
+    // Checkbox "Ver apenas UC's seleccionada"
+    // Esconde uc's não seleccionada
+    // Esconde titulo e headers da tabela caso nenhuma uc's desse semestre esteja seleccionado
+    // Reorganiza cor das linhas depois de escondidas as uc's não seleccionadas
+    $('#select').click(function () {
+        seleccionar();
+        desactivarTitulo();
+        corLinhas();
+    });
+    
+    // Guarda as opções selecionadas mesmo que o chrome seja encerrado
+    // uso da API do chrome storage
+    $('#select').change(function () {
+            chrome.storage.sync.set({[this.id] : document.getElementById(this.id).checked});
+    });
+    for (i = 0; i < ucs_1_semestre.length; i++) {
+        $('#checkbox' + ucs_1_semestre[i][0] + '').change(function () {
+            chrome.storage.sync.set({[this.id] : document.getElementById(this.id).checked});
+        });
+    }
+    for (i = 0; i < ucs_2_semestre.length; i++) {
+        $('#checkbox' + ucs_2_semestre[i][0] + '').change(function () {
+            chrome.storage.sync.set({[this.id] : document.getElementById(this.id).checked});
+        });
+    }
+    
+    // Carrega as opções guardadas no chrome storage
+        // Primeiro Semestre
+    var cbox1 = [];
+    for (i = 0; i < ucs_1_semestre.length; i++) {
+        cbox1.push('checkbox' + ucs_1_semestre[i][0]);   
+    }
+    chrome.storage.sync.get(cbox1, function(val) {
+        for (i = 0; i < ucs_1_semestre.length; i++) {
+            document.getElementById(cbox1[i]).checked = val[cbox1[i]];
+        }
+    });
+        // Segundo Semestre
+    var cbox2 = [];
+    for (i = 0; i < ucs_2_semestre.length; i++) {
+        cbox2.push('checkbox' + ucs_2_semestre[i][0]);   
+    }
+    chrome.storage.sync.get(cbox2, function(val) {
+        for (i = 0; i < ucs_2_semestre.length; i++) {
+            document.getElementById(cbox2[i]).checked = val[cbox2[i]];
+        }
+    });
+         // Opção de Ver apenas UC's seleccionadas
+    chrome.storage.sync.get('select', function(val) {
+        document.getElementById('select').checked = val['select'];
+        if (val['select']) {
+            seleccionar();
+            desactivarTitulo();
+            corLinhas();
+        }
+    });
+    
+    // Filtra em tempo real as UC's à medida que se vai escrevendo na caixa de pesquisa
+    // Converte letras acentuadas em não acentuadas para facilitar pesquisa
+    var $rows = $('tbody tr');
+    $('#search').keyup(function () {
+        document.getElementById('select').checked = false;
+        
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').latinise().toLowerCase();
+            $rows.show().filter(function () {
+            var text = $(this).text().replace(/\s+/g, ' ').latinise().toLowerCase();
+            return !~text.indexOf(val);
+        }).hide();
+        
+        desactivarTitulo();
+        corLinhas();
+    }); 
+});
+
+// Função que esconde as linhas das UC's que não estiverem seleccionadas
+function seleccionar () {
+    if (document.getElementById('select').checked) {
+        for (i = 0; i < ucs_1_semestre.length; i++) {
+            var cb = "checkbox" + ucs_1_semestre[i][0];
+            if (!document.getElementById(cb).checked) {
+                $('#' + ucs_1_semestre[i][0] + '').hide(); 
+            }
+        }
+        for (i = 0; i < ucs_2_semestre.length; i++) {
+            var cb = "checkbox" + ucs_2_semestre[i][0];
+            if (!document.getElementById(cb).checked) {
+                $('#' + ucs_2_semestre[i][0] + '').hide(); 
+            }
+        }
+    } else {
+        $('.linhas').show();
+    }
+}
+
+// Função que alterna a cor do fundo das linhas
+// Função é chamada ao carregar a pagina ou depois de uma filtragem para actualizar
+function corLinhas () {
+    var linhas1 = $('#primeiro tbody tr:visible');
+        for (i = 0; i < linhas1.length; i++) {
+            var linha1 = linhas1[i];
+            if (i%2 != 0) {
+                linha1.style.background = '#dde1e9';
+            } else {
+                linha1.style.background = '#ffffff';
+            }
+        }
+        
+        var linhas2 = $('#segundo tbody tr:visible');
+        for (i = 0; i < linhas2.length; i++) {
+            var linha2 = linhas2[i];
+            if (i%2 != 0) {
+                linha2.style.background = '#dde1e9';
+            } else {
+                linha2.style.background = '#ffffff';
+            }
+        }
+}
+
+// Função que esconde o nome e o cabeçalho da tabela dos semestres quando
+// nenhuma linha da tabela está visivel
+function desactivarTitulo () {
+    if ($('#primeiro tbody tr:visible').length == 0) {
+            $('.primeiro').hide();  
+        } else {
+            $('.primeiro').show();
+        }
+        
+        if ($('#segundo tbody tr:visible').length == 0) {
+            $('.segundo').hide();   
+        } else {
+            $('.segundo').show();
+        }
+}              
---
+++ ucs.js
@@ -0,0 +1,41 @@
+var ano = "2016-2017";
+
+var ucs_1_semestre = [
+    ['21002',   'Álgebra Linear I',                                     '31-01-2017','T',    '28-07-2017','T'],
+    ['21010',   'Arquitectura de Computadores',                         '15-02-2017','T',    '21-07-2017','T'],
+    ['21157',   'Cálculo para Informática',                             '23-01-2017','T',    '24-07-2017','M'],
+    ['21020',   'Computação Gráfica',                                   '13-02-2017','T',    '21-07-2017','M'],
+    ['21021',   'Computação Numérica',                                  '08-02-2017','T',    '28-07-2017','M'],
+    ['21041',   'Estatística Aplicada I',                               '14-02-2017','T',    '18-07-2017','M'],
+    ['21045',   'Estruturas de Dados e Algoritmos Avançados',           '22-02-2017','M',    '24-07-2017','T'],
+    ['21048',   'Física Geral',                                         '14-02-2017','T',    '27-07-2017','T'],
+    ['21053',   'Fundamentos de Bases de Dados',                        '07-02-2017','T',    '20-07-2017','M'],
+    ['21062',   'Gestão de Projetos Informáticos',                      '17-02-2017','T',    '18-07-2017','T'],
+    ['21077',   'Linguagens de Programação',                            '24-01-2017','M',    '17-07-2017','T'],
+    ['21078',   'Linguagens e Computação',                              '20-02-2017','T',    '14-07-2017','M'],
+    ['21089',   'Processos Estocásticos Aplicados',                     '01-02-2017','T',    '26-07-2017','T'],
+    ['21090',   'Programação',                                          '06-02-2017','T',    '26-07-2017','M'],
+    ['21103',   'Sistemas de Gestão de Bases de Dados',                 '26-01-2017','M',    '13-07-2017','T'],
+    ['21106',   'Sistemas em Rede',                                     '30-01-2017','T',    '19-07-2017','T'],
+    ['21110',   'Sistemas Multimédia',                                  '10-02-2017','T',    '13-07-2017','M']
+];
+
+var ucs_2_semestre = [
+    ['21001',   'Administração de Sistemas Informáticos',               '09-06-2017','T',    '29-09-2017','M'],
+    ['21007',   'Análise de Sistemas',                                  '26-06-2017','M',    '29-09-2017','T'],
+    ['21018',   'Compilação',                                           '26-06-2017','T',    '26-09-2017','T'],
+    ['21025',   'Desenvolvimento do Software',                          '12-07-2017','T',    '22-09-2017','T'],
+    ['21163',   'Elementos de Estatística Multivariada',                '03-07-2017','M',    '22-09-2017','T'],
+    ['21037',   'Elementos de Probabilidades e Estatística',            '19-06-2017','T',    '05-09-2017','T'],
+    ['21043',   'Estatística Computacional',                            '16-06-2017','T',    '28-09-2017','M'],
+    ['21046',   'Estruturas de Dados e Algoritmos Fundamentais',        '27-06-2017','M',    '15-09-2017','M'],
+    ['21071',   'Introdução à Inteligência Artificial',                 '07-07-2017','T',    '27-09-2017','T'],
+    ['21073',   'Introdução à Probabilidade e Estatística Bayesianas',  '11-07-2017','T',    '08-09-2017','T'],
+    ['21076',   'Investigação Operacional',                             '23-06-2017','M',    '06-09-2017','T'],
+    ['21082',   'Matemática Finita',                                    '09-06-2017','M',    '13-09-2017','M'],
+    ['21093',   'Programação por Objetos',                              '10-07-2017','M',    '26-09-2017','M'],
+    ['21097',   'Raciocínio e Representação do Conhecimento',           '22-06-2017','M',    '14-09-2017','T'],
+    ['21108',   'Sistemas Distribuídos',                                '06-07-2017','T',    '05-09-2017','M'],
+    ['21109',   'Sistemas e Serviços Web',                              '16-06-2017','M',    '19-09-2017','M'],
+    ['21111',   'Sistemas Operativos',                                  '03-07-2017','T',    '18-09-2017','T']
+];
